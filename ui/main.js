23facd96dedb5dc38caf117960446f3de1d51f00
sparadeshi <30679725+sparadeshi@users.noreply.github.com>
2017-08-30 22:42:51 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,13 @@ element.innerHTML = 'Hi! This is Stevenson Paradeshi - Change Text';
 
 //Move the image
 var img=document.getElementById('madi');
+var marginLeft = 0;
+function moveRight (){
+    marfinLeft =marginLeft +10;
+    img.style.marginLeft - marginLeft + 'px';
+}
 img.onclick = function (){
+    var interval = setInterval (moveRight, 100);
     img.style.marginLeft = '100px';
     
 };
