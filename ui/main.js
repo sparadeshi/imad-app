413fcd97e28edeba1288e79366bfcbcfb8374212
sparadeshi <30679725+sparadeshi@users.noreply.github.com>
2017-08-30 22:46:24 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Hi! This is Stevenson Paradeshi - Change Text';
 
 //Move the image
-var img=document.getElementById('madi');
+var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight (){
-    marginLeft - marginLeft +10;
-    img.style.marginLeft - marginLeft + 'px';
+    marginLeft - marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function (){
     var interval = setInterval (moveRight, 100);
