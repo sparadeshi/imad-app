3b4ac5a7c53eccbab1ae057e9824474353455d97
sparadeshi <30679725+sparadeshi@users.noreply.github.com>
2017-08-30 22:34:41 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //Change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'Hi! This is Stevenson Paradeshi - Change Text';
+
+//Move the image
+var img=document.getElementById('img');
+img.onclick = function (){
+    img.style.marginLeft = '100px';
+    
+};
