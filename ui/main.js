053733a72933829a5d4236322b1a9ded8ea54cdd
sparadeshi <30679725+sparadeshi@users.noreply.github.com>
2017-08-30 22:31:05 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,4 @@
 console.log('Loaded!');
+//Change the text of the main-text div
+var element = document.getElementById('main-text');
+element.innerHTML = 'Hi! This is Stevenson Paradeshi - Change Text';
